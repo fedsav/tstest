fe6faad731c4546dde7c66e69f258c835a289931
Fedor <fed-sav@yandex.ru>
2023-11-09 20:09:50 +0300

    Удалены элементы массива, служившие для примера

--- src/App.vue
+++ src/App.vue
@@ -5,22 +5,7 @@ import { reactive } from 'vue';
 
 
 
-let workers = reactive ([
-  {
-    name: 'Alex',
-    surname: 'Moscoff',
-    exp: 2,
-    age: 28,
-    adress: 'Russi Moscow Kolotushkina 22, 42'
-  },
-  {
-    name: 'Lexa',
-    surname: 'Peterec',
-    exp: 4,
-    age: 23,
-    adress: 'Russi Spb Kalova 56'
-  }
-])
+let workers = reactive ([])
 </script>
 
 <template>
